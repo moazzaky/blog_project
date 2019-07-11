74cd5e837ac3a2e1b83095c737d689fe8a6539d8
moaz_zaky <moaz_khairy@yahoo.com>
2019-07-11 03:00:57 +0200

    home css modified

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'blog\post_detail.html'
+    template_name = 'blog/post_detail.html'
     pk_url_kwarg = 'pk'                        # you can change this to whatever u like.
 
     # context_object_name =                     ...........Name of ur choice
